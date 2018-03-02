af5cc17f13e11d22106f2fa28563e5acaf7bf01b
subin <subibi21@naver.com>
2018-03-02 14:23:24 +0900

    Update hotword.py

--- detector/hotword.py
+++ detector/hotword.py
@@ -2,23 +2,18 @@ import snowboydecoder
 import sys
 import signal
 import os.path
-import wiringpi
 
 class hotword:
     def __init__(self):
         self.PUSH_BUTTON = 14
         self.interrupted = False
         self.model = '/resources'.join([os.path.dirname(__file__), '/HARU.pmdl'])
-        wiringpi.wiringPiSetupGpio()
-        wiringpi.pinMode(self.PUSH_BUTTON, wiringpi.GPIO.INPUT)
-        wiringpi.pullUpDnControl(self.PUSH_BUTTON, wiringpi.GPIO.PUD_UP)
 
     def signal_handler(self, signal, frame):
         self.interrupted = True
 
     def interrupt_callback(self):
-        return wiringpi.digitalRead(self.PUSH_BUTTON) == 0
-
+        return 0
     def start_detection(self, callback_func):
         signal.signal(signal.SIGINT, self.signal_handler)
         self.detector = snowboydecoder.HotwordDetector(self.model, sensitivity=0.5)
